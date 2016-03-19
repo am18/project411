ff8f2f1bae0518bc7e1ffe4dc084cced9200cd4b
patels95 <patels326@gmail.com>
2016-03-19 15:31:44 -0400

    assignment 4

--- routes/index.js
+++ routes/index.js
@@ -3,30 +3,44 @@ var router = express.Router();
 
 //var mongoose = require('mongoose');
 
-var request = require("request");
+var request = require('request');
 var obj;
-var options = { method: 'GET',
-    url: 'http://api.brewerydb.com/v2/search/',
-    qs:
-    { key: 'fcacff0f686f8721f8831c53d8d5b2d2',
-        q: 'miller',
-        type: 'beer' },
-    headers:
-    { 'postman-token': 'f2af4ae2-3be0-0eda-0e25-35fc1e2c0dc2',
-        'cache-control': 'no-cache' } };
+var data;
+var beers = [];
 
-request(options, function (error, response, body) {
-    if (error) throw new Error(error);
+function search(input) {
+    var options = { method: 'GET',
+        url: 'http://api.brewerydb.com/v2/search/',
+        qs:
+        { key: 'fcacff0f686f8721f8831c53d8d5b2d2',
+            q: input,
+            type: 'beer' },
+        headers:
+        { 'postman-token': 'f2af4ae2-3be0-0eda-0e25-35fc1e2c0dc2',
+            'cache-control': 'no-cache' } };
 
-    console.log(body);
-    obj = JSON.parse(body);
-    //console.log(obj.totalResults);
-});
+    request(options, function (error, response, body) {
+        if (error) throw new Error(error);
+
+        obj = JSON.parse(body);
+        data = obj['data'];
+        for(i = 0; i < data.length; i++) {
+            beers.push(data[i]['name']);
+            console.log(beers[i]);
+        }
+    });
+}
 
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
     res.render('index', { title: 'Express' });
+});
+
+router.post('/', function(req, res) {
+    console.log(req.body.input);
+    search(req.body.input);
+    res.render('index', { title: 'Express', beers: beers });
 });
 
 module.exports = router;
