46765bae601f7051f154d38e7320e551d3b4dd12
patels95 <patels326@gmail.com>
2016-03-30 01:11:26 -0400

    login with facebook working

--- config/passport.js
+++ config/passport.js
@@ -4,13 +4,52 @@ var FacebookStrategy = require('passport-facebook').Strategy;
 
 module.exports = function (passport) {
 
+    passport.serializeUser(function(user, done){
+        done(null, user.id);
+    });
+
+    passport.deserializeUser(function(id, done){
+        User.findById(id, function(err, user){
+            done(err, user);
+        });
+    });
+
     passport.use(new FacebookStrategy({
             clientID: configAuth.facebookAuth.clientID,
             clientSecret: configAuth.facebookAuth.clientSecret,
-            callbackURL: configAuth.facebookAuth.callbackURL
+            callbackURL: configAuth.facebookAuth.callbackURL,
+            profileFields: ['id', 'name', 'email']
         },
         function(accessToken, refreshToken, profile, done) {
+            process.nextTick(function() {
+                User.findOne({'id': profile.id}, function(err, user){
+                    if (err) {
+                        return done(err);
+                    }
+                    if (user) {
+                        return done(null, user);
+                    }
+                    else {
+                        var newUser = new User();
+                        newUser.id = profile.id;
+                        newUser.token = accessToken;
+                        newUser.firstName = profile.name.givenName;
+                        newUser.lastName = profile.name.familyName;
+                        newUser.email = profile.emails[0].value;
 
+                        newUser.save(function(err) {
+                            if (err) {
+                                throw err;
+                            }
+                            else {
+                                return done(null, newUser);
+                            }
+                        });
+                        console.log(profile);
+                    }
+                });
+                console.log('logged in');
+            });
         }
     ));
 };
