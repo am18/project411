70053f03be10a7eaa258883cb4e001b48768c6df
patels95 <patels326@gmail.com>
2016-04-06 00:36:52 -0400

    updated beer search

--- routes/index.js
+++ routes/index.js
@@ -2,13 +2,14 @@ var express = require('express');
 var router = express.Router();
 var configAuth = require('../config/auth');
 var Beer = require('../models/beer');
+var Search = require('../models/search');
 
 //var mongoose = require('mongoose');
 
 var request = require('request');
 var obj;
 var data;
-var beers = [];
+var beerNames = [];
 
 function search(input) {
 
@@ -22,23 +23,82 @@ function search(input) {
         { 'postman-token': 'f2af4ae2-3be0-0eda-0e25-35fc1e2c0dc2',
             'cache-control': 'no-cache' } };
 
-    request(options, function (error, response, body) {
-        if (error) throw new Error(error);
-        obj = JSON.parse(body);
-        data = obj['data'];
-        for(i = 0; i < data.length; i++) {
-            beers.push(data[i]['name']);
-            console.log(beers[i]);
+    Search.findOne({'term': input}, function(err, search){
+        if (err) {
+            handleError(err);
+        }
+        if (search){
+            // get all beers with search.term
+            console.log('search mongo database');
+            Beer.find({'name': new RegExp(input+'+', "i")}, function(err, beers){
+                if (err) {
+                    handleError(err);
+                }
+                if (beers) {
+                    console.log(beers);
+                }
+                else {
+                    console.log('no beers found');
+                }
+            });
+        }
+        else {
+            // add new search term to database
+            console.log('new search term');
+            var newSearch = new Search();
+            newSearch.term = input;
 
-            // check if beer is already in database
-            Beer.findOne({});
+            newSearch.save(function(err) {
+                if (err) {
+                    throw err;
+                }
+            });
 
+            request(options, function (error, response, body) {
+                if (error) throw new Error(error);
+                obj = JSON.parse(body);
+                data = obj['data'];
+                for(i = 0; i < data.length; i++) {
+                    var id = data[i]['id'];
+                    var name = data[i]['name'];
+                    var description = data[i]['description'];
+                    var abv = data[i]['abv'];
+                    if (data[i].hasOwnProperty('labels')) {
+                        var image = data[i]['labels']['large'];
+                    }
 
-            //var newBeer =
+                    beerNames.push(name);
+                    console.log(beerNames[i]);
+
+                    // check if beer is already in database
+                    Beer.findOne({'id': id}, function(err, beer){
+                        if (err) {
+                            handleError(err);
+                        }
+                        if (beer) {
+                            // beer is already in database
+                        }
+                        else {
+                            // add new beer to database
+                            var newBeer = new Beer();
+                            newBeer.id = id;
+                            newBeer.name = name;
+                            newBeer.description = description;
+                            newBeer.abv = abv;
+                            newBeer.image = image;
+
+                            newBeer.save(function(err) {
+                                if (err) {
+                                    throw err;
+                                }
+                            });
+                        }
+                    });
+                }
+            });
         }
     });
 }
-
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -53,7 +113,7 @@ router.post('/', function(req, res) {
     else {
         // display error using connect-flash
     }
-    res.render('index', { title: 'BeerBuddy', beers: beers });
+    res.render('index', { title: 'BeerBuddy', beers: beerNames });
 });
 
 module.exports = router;
